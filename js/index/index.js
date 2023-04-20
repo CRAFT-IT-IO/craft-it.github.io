bcd8e6123a4df08bab4ee9330b9b7b35bd235794
Luis Blacio <luis.blacio@craft-it.io>
2023-04-20 18:52:34 +0200

    home mobile version

--- js/index/index.js
+++ js/index/index.js
@@ -1,3 +1,4 @@
+
 $(function () {
     let logoDotContainer = $('logo-dot:not([data-display-mode])');
     let logoDotContainerRightSide = $('logo-dot[data-display-mode]');
@@ -13,73 +14,80 @@ $(function () {
     setTimeout(function () {
         logoContainer.show().addClass('animated');
         //setTimeout(function () {
-            initContentMenu(['I.T. Lab FOR BESKOPEN SOLUTION IN RISK MANAGEMENT'], { delay: 40, withAnimation: true, contentDirection: 'bottom' }, function () {
-                logoDotContainerRightSide.addClass('animated');
-                addHeader();
-                let nextPageButton = $('<div></div>', { class: 'btn-next-page' })
-                    .append($('<i></i>', { class: 'arrow-down fa fa-chevron-down', 'aria-hidden': true, alt: 'next-section' }));
+        initContentMenu(['I.T. Lab FOR BESKOPEN SOLUTION IN RISK MANAGEMENT'], { delay: 40, withAnimation: true, contentDirection: 'bottom' },
+            function () {
+            //logoDotContainerRightSide.addClass('animated');
+            //addHeader();
+            //let nextPageButton = $('<div></div>', { class: 'btn-next-page' })
+            //    .append($('<i></i>', { class: 'arrow-down fa fa-chevron-down', 'aria-hidden': true, alt: 'next-section' }));
 
-                $('.craft-footer ').prepend(nextPageButton);
+            //$('.craft-footer ').prepend(nextPageButton);
 
-                var isSCrolling = false;
-                let arrow = $('.arrow-down');
-                let footerAnimations = $('.craft-footer .footer-item');
-                let animationCount = 0;
-                for (var i = 0; i < footerAnimations.length; i++) {
-                    let animationIndex = $(footerAnimations[i]).attr('animation-order');
-                    if (animationIndex > animationCount)
-                        animationCount = animationIndex;
-                }
+            //var isSCrolling = false;
+            //let footerAnimations = $('.craft-footer .footer-item');
+            //let animationCount = 0;
+            //for (var i = 0; i < footerAnimations.length; i++) {
+            //    let animationIndex = $(footerAnimations[i]).attr('animation-order');
+            //    if (animationIndex > animationCount)
+            //        animationCount = animationIndex;
+            //}
 
-                let currentAnimationIndex = 0;
-                arrow.on('click', function () {
-                    isSCrolling = true;
-                    footerAnimation(-1);
-                });
+            //let currentAnimationIndex = 0;
+            //let arrow = $('.arrow-down');
+            //arrow.on('click', function () {
+            //    isSCrolling = true;
+            //    footerAnimation(-1);
+            //});
 
-                $(window).on('wheel', function (e) {
-                    if (isSCrolling)
-                        return;
+            //$(window).on('wheel', function (e) {
+            //    if (isSCrolling)
+            //        return;
 
-                    isSCrolling = true;
-                    let delta = getDelta(e);
-                    footerAnimation(delta);                    
-                });
+            //    isSCrolling = true;
+            //    let delta = getDelta(e);
+            //    footerAnimation(delta);
+            //});
 
-                function footerAnimation(delta) {
+            //function footerAnimation(delta) {
+            //    let animationCount = 0;
+            //    for (var i = 0; i < footerAnimations.length; i++) {
+            //        let animationIndex = $(footerAnimations[i]).attr('animation-index');
+            //        if (animationIndex > animationCount)
+            //            animationCount = animationIndex;
+            //        else if (animationIndex == animationCount) {
+            //            let currentTop = $(footerAnimations[i]);
+            //            let oldTop = $(footerAnimations[i - 1]);
 
-                    let animationCount = 0;
-                    for (var i = 0; i < footerAnimations.length; i++) {
-                        let animationIndex = $(footerAnimations[i]).attr('animation-index');
-                        if (animationIndex > animationCount)
-                            animationCount = animationIndex;
-                    }
+            //            if (oldTop != currentTop)
+            //                animationCount++;
+            //        }
+            //    }
 
-                    delta < 0 ? ++currentAnimationIndex /* to down*/ : --currentAnimationIndex; /* to up*/
+            //    delta < 0 ? ++currentAnimationIndex /* to down*/ : --currentAnimationIndex; /* to up*/
 
-                    if (currentAnimationIndex < 0)
-                        currentAnimationIndex = 0;
-                    else if (currentAnimationIndex > animationCount)
-                        currentAnimationIndex = animationCount;
+            //    if (currentAnimationIndex < 0)
+            //        currentAnimationIndex = 0;
+            //    else if (currentAnimationIndex > animationCount)
+            //        currentAnimationIndex = animationCount;
 
-                    let container = $('.footer-container');
-                    container.animate({ top: -((currentAnimationIndex * 100)) + '%' }, 1500, function () {
-                        currentAnimationIndex == animationCount ? arrow.hide() : arrow.show();
-                        isSCrolling = false;
-                    });
-                }
+            //    //let container = $('.footer-container');
+            //    //container.animate({ top: -((currentAnimationIndex * 100)) + '%' }, 1500, function () {
+            //    //    currentAnimationIndex == animationCount ? arrow.hide() : arrow.show();
+            //    //    isSCrolling = false;
+            //    //});
+            //}
 
-                function getDelta(e) {
-                    let value;
-                    if (e.originalEvent) {
-                        value = e.originalEvent.wheelDelta || -e.originalEvent.deltaY || -e.originalEvent.detail;
-                    } else {
-                        value = e.wheelDelta || -e.deltaY || -e.detail;
-                    }
+            //function getDelta(e) {
+            //    let value;
+            //    if (e.originalEvent) {
+            //        value = e.originalEvent.wheelDelta || -e.originalEvent.deltaY || -e.originalEvent.detail;
+            //    } else {
+            //        value = e.wheelDelta || -e.deltaY || -e.detail;
+            //    }
 
-                    return Math.max(-1, Math.min(1, value));
-                }
-            });
+            //    return Math.max(-1, Math.min(1, value));
+            //}
+        });
         //}, 2000);
     }, 1000);
 });
