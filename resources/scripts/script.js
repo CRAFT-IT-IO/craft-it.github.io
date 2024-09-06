95d2657647daccbb61191401b245d8c8372c8d63
bartkevd-craft <dziugas.bartkevicius@craft-it.io>
2024-09-06 15:35:59 +0200

    Button to CTA changes
    changes made tot he button appearance and how transition happens/reveals the content of the CTA page.

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,27 +1,37 @@
 var button = document.querySelector(".hero-button");
+var heroBottom = document.querySelector(".hero-bottom");
 var text = document.querySelector(".button-text");
-var svg = document.querySelector(".arrow-path");
-var contact = document.querySelector(".call-to-action");
-var ctaArrow = document.querySelector(".cta-top-svg");
+var svgArrow = document.querySelector(".svg-arrow");
+var svgPath = document.querySelector(".arrow-path");
 
 button.addEventListener("mouseover", funcIn, false);
 button.addEventListener("mouseout", funcOut, false);
 button.addEventListener("click", kickOff, false);
-ctaArrow.addEventListener("click", kickOff, false);
 
 function funcIn()
 {
-    button.setAttribute("style", "background: #F0ECE9;color: black;")
-    text.setAttribute("style", "color: black;")
-    svg.style.stroke = "black"
+    // button.setAttribute("style", "background: #F0ECE9;color: black;")
+    // text.setAttribute("style", "color: black;")
+    // svg.style.stroke = "black"
 }
 function funcOut()
 {  
-   button.setAttribute("style", "color: #F0ECE9;")
-   text.setAttribute("style", "color: #F0ECE9;")
-   svg.style.stroke = "#F0ECE9"
+//    button.setAttribute("style", "color: #F0ECE9;")
+//    text.setAttribute("style", "color: #F0ECE9;")
+//    svg.style.stroke = "#F0ECE9"
 }
 function kickOff()
 {
-    contact.classList.toggle("active");
+    button.classList.toggle("active");
+    heroBottom.classList.toggle("active");
+    svgArrow.classList.toggle("active");
+
+    // contact.classList.toggle("active");
+    // button.classList.toggle("active");
+    // if(button.classList.contains("active")){
+    //     console.log("yes")
+    //     button.setAttribute("style", "color: #F0ECE9;")
+    //     text.setAttribute("style", "color: #F0ECE9;")
+    //     svg.style.stroke = "#F0ECE9"
+    // }
 }
