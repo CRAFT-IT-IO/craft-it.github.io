f0c989d191c03e0040da02c8e0572227232c91f4
Kevin <kevdelval@gmail.com>
2024-09-10 13:41:07 +0200

    CTA improvement & responsive corrections

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -3,6 +3,8 @@ var heroBottom = document.querySelector(".hero-bottom");
 var text = document.querySelector(".button-text");
 var svgArrow = document.querySelector(".svg-arrow");
 var svgPath = document.querySelector(".arrow-path");
+var callToAction = document.querySelector(".call-to-action");
+var mainContent = document.querySelector("main"); // Sélectionne l'élément 'main'
 
 button.addEventListener("click", () => {
     button.classList.toggle("active");
@@ -10,9 +12,10 @@ button.addEventListener("click", () => {
     svgArrow.classList.toggle("active");
     callToAction.classList.toggle("active");
 
+    // Applique ou enlève la classe 'no-scroll' sur 'main' plutôt que sur 'body'
     if (callToAction.classList.contains("active")) {
-        document.body.classList.add("no-scroll");
+        mainContent.classList.add("no-scroll");
     } else {
-        document.body.classList.remove("no-scroll");
+        mainContent.classList.remove("no-scroll");
     }
 });
