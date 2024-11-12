c9e5dc181a22661f2b821af27c4d781e46363ce5
Luis Blacio <luis.blacio@craft-it.io>
2024-11-12 22:25:35 +0100

    .

--- resources/scripts/menu.js
+++ resources/scripts/menu.js
@@ -1,6 +1,11 @@
-function toggleMenu() {
-	$('.menu').toggleClass('show');
-}
+$(document).ready(function () {
+	$('.menu-item-selected, .toggle-menu, .arrow-back').on('click', (e) => {
+		$('.menu').toggleClass('show');
+		e.preventDefault();
+		e.stopPropagation();
+	});
+});
+
 
 document.addEventListener('scroll', function () {
 	let header = document.querySelector('header');
--- resources/scripts/sticky.js
+++ resources/scripts/sticky.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
-	let header = $('header');
-	// Set the value of the --sticky-container-wrap-top variable
-	document.documentElement.style.setProperty('--sticky-container-wrap-top'/*, header.outerHeight()*/ + 'px');
+	//let header = $('header');
+	//// Set the value of the --sticky-container-wrap-top variable
+	//document.documentElement.style.setProperty('--sticky-container-wrap-top'/*, header.outerHeight()*/ + 'px');
 
-	let subtitleHeight = $('.process-grid').siblings('.section-subtitle').outerHeight();
-	let pos = header.outerHeight() + subtitleHeight;
-	document.documentElement.style.setProperty('--sticky-process-grid-top', pos + 'px');
+	//let subtitleHeight = $('.process-grid').siblings('.section-subtitle').outerHeight();
+	//let pos = header.outerHeight() + subtitleHeight;
+	//document.documentElement.style.setProperty('--sticky-process-grid-top', pos + 'px');
 });
